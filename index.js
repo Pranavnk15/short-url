bbffd881fb81d131c1196882734c885b97340a4f
Pranav <khandagalepranav1546@gmail.com>
2024-07-07 00:49:20 +0530

    added stateful authentication

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const urlRoute = require('./routes/url');
 const URL = require('./models/url');
 const path = require('path');
 const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+const cookieParser = require("cookie-parser");
+const {restrictToLoggedinUserOnly, checkAuth} = require('./middlewares/auth');
+
 
 const app = express();
 const PORT = 8001;
@@ -17,19 +21,22 @@ app.set("view engine", "ejs");
 app.set('views', path.resolve("./views"));
 
 
-
+//middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
 
 //Routes
-app.use('/url', urlRoute);
-app.use("/", staticRoute);
+//inline middleware, this will only run when the req comes to this url
+app.use('/url',restrictToLoggedinUserOnly, urlRoute);
+app.use("/", checkAuth , staticRoute);
+app.use("/user", userRoute)
 
-app.get("/test", async(req, res) => {
-    const allUrls = await URL.find({});
-    return res.render('home', {
-        urls: allUrls,
-    });
-})
+// app.get("/test", async(req, res) => {
+//     const allUrls = await URL.find({});
+//     return res.render('home', {
+//         urls: allUrls,
+//     });
+// })
 
 app.listen(PORT, ()=> console.log(`Server started on Port: ${PORT}`));
